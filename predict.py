957f9e11733b4c29db2b4d98a69db3d47980007c
Gabriella Hawkes <ghawkes.dev@gmail.com>
2026-01-31 18:57:59 -0600

    Clean code

--- predict.py
+++ predict.py
@@ -8,20 +8,14 @@ y_train = np.load("y_train.npy")
 x_test = np.load("x_test.npy")
 y_test = np.load("y_test.npy")
 
-# plt.scatter(x_train, y_train)
-# plt.show()
-
-# plt.scatter(x_test, y_train)
-# plt.show()
-
 
 def learn(num_iter):
     alpha = 0.001
-    slope = 10
+    slope = 1 # Seed value
     slope_history = []
     bias_history = []
     error_history = []
-    bias = 1
+    bias = 1 # Seed value
     params = np.array([slope, bias])
 
     j = 0
@@ -29,7 +23,6 @@ def learn(num_iter):
     while j < num_iter:
         x_j_input = x_train[j % len(x_train)]
         input_j = np.array([x_j_input, 1]) # Use 1 as the x input val for bias
-        y_j = y_train[j % len(x_train)]
 
         # Calculate sum of loss
         loss_sum = 0
@@ -52,10 +45,23 @@ def learn(num_iter):
 
 learned_slope, bias, slope_history, error_history = learn(200)
 
-print(f"Equation is slope = {learned_slope} and bias is {bias}")
+print(f"Learned slope: {learned_slope}")
+print(f"Learned bias: {bias}")
+print(f"Equation: h(x) = {learned_slope}x + {bias}")
+
+plt.scatter(x_train, y_train, color="blue")
+plt.scatter(x_test, y_test, color="orange")
+plt.title("Training and Test Data")
+plt.show()
 
 plt.scatter(x_train, y_train)
 plt.plot([-2, 0,2], [bias - 2 * learned_slope, bias, bias + 2 * learned_slope], color="yellow")
+plt.title("Model with Training Data")
+plt.show()
+
+plt.scatter(x_test, y_test)
+plt.plot([-1, 0,1], [bias - learned_slope, bias, bias + learned_slope], color="yellow")
+plt.title("Model with Test Data")
 plt.show()
 
 plt.scatter([i for i in range(0, len(slope_history))], slope_history)
