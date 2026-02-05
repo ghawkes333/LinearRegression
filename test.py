1e81fb5ec79b52b228c96d2617886a9137cfff2c
Gabriella Hawkes <ghawkes.dev@gmail.com>
2026-02-05 15:45:47 -0600

    Use weighted input

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = [1, 1, 1]
-y = [[1,1,1], [1,1,1]]
-print(np.shape(y))
+# x = [1, 1, 1]
+# y = [[1,1,1], [1,1,1]]
+# print(np.shape(y))
 
-print([1,2] * [10,10])
+# print([1,2] * [10,10])
+
+print(0 ** 0)
